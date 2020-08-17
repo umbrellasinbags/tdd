0bc1c74dc6dfe6499009593d60a1e16f91a6c85d
venugv01 <venugv01@stepstone.com>
2020-08-17 17:02:33 +0200

    Tests for tdd basics, chapter 1

--- src/main/java/co/interleap/courses/tdd/VowelCounter.java
+++ src/main/java/co/interleap/courses/tdd/VowelCounter.java
@@ -1,4 +1,7 @@
 package co.interleap.courses.tdd;
 
 public class VowelCounter {
+	int countNumberOfVowels(String s) {
+		return s.length();
+	}
 }
--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -1,9 +1,23 @@
 package co.interleap.courses.tdd;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class VowelCounterTest {
 
     @Test
-    public void dummy(){}
+    public void testNoOfVowelsInEmptyString(){
+        VowelCounter vowelCounter = new VowelCounter();
+        Assert.assertEquals(0, vowelCounter.countNumberOfVowels(""));
+    }
+    @Test
+    public void testNoOfVowelsInSingleVowelString(){
+        VowelCounter vowelCounter = new VowelCounter();
+        Assert.assertEquals(1, vowelCounter.countNumberOfVowels("i"));
+    }
+    @Test
+    public void testNoOfVowelsInAllVowelString(){
+        VowelCounter vowelCounter = new VowelCounter();
+        Assert.assertEquals(7, vowelCounter.countNumberOfVowels("aeiouae"));
+    }
 }
